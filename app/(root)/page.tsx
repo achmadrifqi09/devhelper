67b1697b29d3e0cab14ba8f14681e4b3ef9c6a21
achmadrifqi09 <achmadrifqi09@gmail.com>
2025-07-28 15:25:42 +0700

    init branch main

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,7 @@ import HomeClient from "@/components/home-client";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Postman Collection Converter | APIu",
+    title: "Devhelper",
     description:
         "A simple way to convert a postman collection into an open API schema with just a few clicks.",
 };
